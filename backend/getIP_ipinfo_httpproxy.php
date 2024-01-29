b64a7f110fcd4f6508a8a6415a4e60c9960e6d75
Huynh Le Nghia <117708194+nghiahl-sysadmin@users.noreply.github.com>
2024-01-29 23:23:18 +0700

    Create getIP_ipinfo_httpproxy.php

---
+++ backend/getIP_ipinfo_httpproxy.php
@@ -0,0 +1,16 @@
+<?php
+$IPINFO_PROXYADDRESS="proxy.hcm.fpt.vn"; // leave proxy address blank for direct
+$IPINFO_PROXYPORT="80";
+
+$streamContext = array(
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+    ),
+    'http' => array(
+        'proxy'           => 'tcp://'.$IPINFO_PROXYADDRESS.':'.$IPINFO_PROXYPORT,
+        'request_fulluri' => true,
+    ),
+);
+$IPINFO_HTTPPROXY= stream_context_create($streamContext);
+?>
