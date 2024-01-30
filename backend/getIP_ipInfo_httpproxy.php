ffe0216f1494b0b892542e73ba5b6165355a9adf
Huynh Le Nghia <117708194+nghiahl-sysadmin@users.noreply.github.com>
2024-01-30 10:54:19 +0700

    Update and rename getIP_ipinfo_httpproxy.php to getIP_ipInfo_httpproxy.php

--- backend/getIP_ipInfo_httpproxy.php
+++ backend/getIP_ipInfo_httpproxy.php
@@ -1,6 +1,6 @@
 <?php
-$IPINFO_PROXYADDRESS = ''; // leave proxy address blank for direct
-$IPINFO_PROXYPORT = '3128';
+$IPINFO_PROXYADDRESS=''; // leave proxy address blank for direct
+$IPINFO_PROXYPORT='';
 
 $streamContext = array(
     'ssl' => array(
