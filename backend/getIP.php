2ef7dd4748c886ddcc6a9c570f54ff30981d06ae
Huynh Le Nghia <117708194+nghiahl-sysadmin@users.noreply.github.com>
2024-01-30 11:14:01 +0700

    Update getIP.php

--- backend/getIP.php
+++ backend/getIP.php
@@ -78,10 +78,7 @@ function getIpInfoTokenString()
 
     return '?token=' . $IPINFO_APIKEY;
 }
-$httpproxyFile="getIP_ipInfo_httpproxy.php";
-if (!file_exists($httpproxyFile)) return "";
-require $httpproxyFile;
-if (empty($IPINFO_PROXYADDRESS)) $IPINFO_HTTPPROXY = $NULL;
+
 /**
  * @param string $ip
  *
@@ -89,8 +86,7 @@ if (empty($IPINFO_PROXYADDRESS)) $IPINFO_HTTPPROXY = $NULL;
  */
 function getIspInfo($ip)
 {
-    $json = file_get_contents('https://ipinfo.io/' . $ip . '/json'.getIpInfoTokenString(), False, $IPINFO_HTTPPROXY);
-
+    $json = file_get_contents('https://ipinfo.io/' . $ip . '/json' . getIpInfoTokenString());
     if (!is_string($json)) {
         return null;
     }
@@ -213,7 +209,7 @@ function getServerLocation()
         return $serverLoc;
     }
 
-    $json = file_get_contents('https://ipinfo.io/json'.getIpInfoTokenString(), False, $IPINFO_HTTPPROXY);
+    $json = file_get_contents('https://ipinfo.io/json' . getIpInfoTokenString());
     if (!is_string($json)) {
         return null;
     }
