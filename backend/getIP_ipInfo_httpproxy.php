5c1d87f643e26b17a769271decd8626b8881115d
Huynh Le Nghia <117708194+nghiahl-sysadmin@users.noreply.github.com>
2024-01-30 12:00:47 +0700

    Update getIP_ipInfo_httpproxy.php

--- backend/getIP_ipInfo_httpproxy.php
+++ backend/getIP_ipInfo_httpproxy.php
@@ -1,16 +1,16 @@
 <?php
-$IPINFO_PROXYADDRESS = ''; // leave proxy address blank for direct
-$IPINFO_PROXYPORT = '80';
+$IPINFO_PROXYADDRESS=""; // leave proxy address blank for direct
+$IPINFO_PROXYPORT="3128";
 
 $streamContext = array(
-    'ssl' => array(
-        'verify_peer' => false,
-        'verify_peer_name' => false,
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
     ),
     'http' => array(
-        'proxy' => 'tcp://' . $IPINFO_PROXYADDRESS . ':' . $IPINFO_PROXYPORT,
+        'proxy'           => 'tcp://'.$IPINFO_PROXYADDRESS.':'.$IPINFO_PROXYPORT,
         'request_fulluri' => true,
     ),
 );
-$IPINFO_HTTPPROXY = stream_context_create($streamContext);
+$IPINFO_HTTPPROXY= stream_context_create($streamContext);
 ?>
